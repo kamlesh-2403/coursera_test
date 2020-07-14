e6427c8b4c645fc1292c9857a237113bf8ccbd05
kamlesh-2403 <56518617+kamlesh-2403@users.noreply.github.com>
2020-07-14 11:33:39 +0530

    completed

--- digest_cycle/app.js
+++ digest_cycle/app.js
@@ -26,15 +26,41 @@
     //   };
     // }
 
+    //use of settimeout function
+
+    // function AppController1($scope) {
+    //     $scope.counter = 0;
+    //     $scope.upCounter = function () {
+    //         setTimeout(function () {
+    //           $scope.counter++;
+    //           console.log("Increment");
+    //         },200);
+    //     };
+    // }
+
+
+    //use of $digest which can make the clock fire even there is an error or an exception
+
+
+    // function AppController1($scope){
+    //   $scope.counter = 0;
+    //   $scope.upCounter = function () {
+    //       setTimeout(function (){
+    //         $scope.counter++;
+    //         console.log("incremented");
+    //         $scope.$digest;
+    //       },2000);
+    //   };
+    // }
+
+    // use of the appropriate or preffered method for setting up the digest clock
     function AppController1($scope) {
-        $scope.counter = 0;
-        $scope.upCounter = function () {
-            setTimeout(function () {
-              $scope.counter++;
-              console.log("Increment");
-            },200);
-        };
-    }
-
-
+      $scope.counter = 0;
+      setTimeout(function () {
+        $scope.$apply(function () {
+          $scope.counter++;
+          console.log("incremented");
+        });
+      },2000);
+    };
 })();
